d2b40dacf63f3d024cc5159f37df525f70bff018
yuitokiuchi <yuito.kiuchi.dev@gmail.com>
2026-03-15 11:35:10 +0900

    add dist

---
+++ dist/fps_walkthrough_bg.wasm.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wasm_bindgen_func_elem_94570: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_99188: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_89688: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_89704: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_99189: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99189_4: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99189_5: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99189_6: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99189_7: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99189_8: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99189_9: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99189_10: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99190: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_94571: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_start: () => void;
